49ace9fd9e23f6c6e033f81caba937743f49b12e
Panczel24 <pcsenge8@gmail.com>
2025-01-17 12:32:35 +0100

    Osszeadas_kész

--- szamolo/Program.cs
+++ szamolo/Program.cs
@@ -8,8 +8,38 @@ namespace szamolo
 {
 	internal class Program
 	{
+		static float elsoszam = 0;
+		static float masodikszam = 0;
+		static string jelek = "";
+
 		static void Main(string[] args)
 		{
+			Indit();
+		}
+			
+
+
+		static void Indit()
+		{
+				Console.WriteLine("add meg az első számot");
+				elsoszam = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("add meg a második számot");
+				masodikszam = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("add meg a műveleti jelet");
+				jelek = Console.ReadLine();
+
+				switch (jelek)
+				{
+					case "+":
+						Osszeadas();
+						break;
+				}
+		}
+
+		static void Osszeadas()
+		{
+			float osszeadva = elsoszam + masodikszam;
+			Console.WriteLine("A két szám össszege:" + osszeadva);
 		}
 	}
 }
